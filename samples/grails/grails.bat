5e5a1a969107fcf10ee00c4779f59ca858da222f
shvets <shvets@b101193a-aa37-0410-b78f-865dd12e72d7>
2007-08-16 21:48:38 +0000

    git-svn-id: svn://svn.code.sf.net/p/jlaunchpad/code/trunk@11 b101193a-aa37-0410-b78f-865dd12e72d7

--- samples/grails/grails.bat
+++ samples/grails/grails.bat
@@ -4,12 +4,16 @@ if exist "%USERPROFILE%\jlaunchpad\config.bat" (
   @call "%USERPROFILE%\jlaunchpad\config.bat"
 )
 
-SET GRAILS_HOME=C:\Work\jlaunchpad\trunk\samples\grails
+SET GRAILS_HOME=C:\JavaPrograms\grails-0.5.6
+
+SET DIR=%~dp0
 
 SET MAIN_CLASS=org.codehaus.groovy.grails.cli.GrailsScriptRunner
 
-SET PROPERTIES="-deps.file.name=%GRAILS_HOME%\deps.xml" "-main.class.name=%MAIN_CLASS%"
+SET SYSTEM_PROPERTIES="-Dgrails.version=0.5.6"
+
+SET PROPERTIES="-deps.file.name=%DIR%deps.xml" "-main.class.name=%MAIN_CLASS%"
 
 SET APP_NAME=grails
 
-%LAUNCHER_HOME%\launcher.bat %PROPERTIES% %*
+%LAUNCHER_HOME%\launcher.bat %SYSTEM_PROPERTIES% %PROPERTIES% %*
