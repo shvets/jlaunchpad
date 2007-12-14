66333092f1394a608ae3bf37931d73831f4f9663
shvets <shvets@b101193a-aa37-0410-b78f-865dd12e72d7>
2007-12-14 20:57:20 +0000

    restructuring project
    git-svn-id: svn://svn.code.sf.net/p/jlaunchpad/code/trunk@83 b101193a-aa37-0410-b78f-865dd12e72d7

--- launcher/projects/jlaunchpad-launcher/src/main/java/org/sf/jlaunchpad/install/CoreInstaller.java
+++ launcher/projects/jlaunchpad-launcher/src/main/java/org/sf/jlaunchpad/install/CoreInstaller.java
@@ -2,8 +2,8 @@ package org.sf.jlaunchpad.install;
 
 
 import org.sf.jlaunchpad.core.LauncherException;
-import org.sf.jlaunchpad.util.StringUtil;
 import org.sf.jlaunchpad.util.FileUtil;
+import org.sf.jlaunchpad.util.StringUtil;
 import org.sf.pomreader.ProjectInstaller;
 
 import java.io.*;
@@ -16,9 +16,11 @@ import java.io.*;
  */
 public class CoreInstaller {
   public final static String LAUNCHER_PROPERTIES =
-          System.getProperty("user.home") + File.separatorChar + ".jlaunchpad";
+      System.getProperty("user.home") + File.separatorChar + ".jlaunchpad";
 
   protected LauncherProperties launcherProps = new LauncherProperties(LAUNCHER_PROPERTIES);
+
+  private ProjectInstaller installer;
 
   protected void load() throws IOException {
     launcherProps.load();
@@ -40,86 +42,91 @@ public class CoreInstaller {
     try {
       load();
 
-      ProjectInstaller installer = new ProjectInstaller();
+      installer = new ProjectInstaller();
 
-      System.out.println("Installing bootstrap-mini project...");
-      installer.install("projects/bootstrap-mini", false);
+      install("bootstrap-mini");
+      install("jlaunchpad-common");
+      install("classworlds");
+      install("pom-reader");
+      install("jlaunchpad-launcher");
 
-      System.out.println("Installing \"jlaunchpad-common\" project...");
-      installer.install("projects/jlaunchpad-common", false);
-
-      System.out.println("Installing \"classworlds\" project...");
-      installer.install("projects/classworlds", false);
-
-      System.out.println("Installing \"pom-reader\" project...");
-      installer.install("projects/pom-reader", false);      
-
-      System.out.println("Installing \"jlaunchpad-launcher\" project...");
-      installer.install("projects/jlaunchpad-launcher", false);
-
-      File[] files = new File("src/main/config").listFiles();
-
-      String launcherHome = System.getProperty("launcher.home");
-      File launcherHomeFile = new File(launcherHome);
-
-      if(!launcherHomeFile.exists()) {
-        launcherHomeFile.mkdirs();
-      }
-
-      for(File fromFile : files) {
-        String extension = FileUtil.getExtension(fromFile);
-
-        boolean isUnixFile = (extension != null && extension.equals("sh"));
-
-        if(fromFile.exists() && !fromFile.isHidden() && !fromFile.isDirectory()) {
-          File toFile = new File(launcherHomeFile.toString(), fromFile.getName());
-
-          System.out.println("Copying \"" + fromFile.getName() + "\" file to \"" + toFile.getParent() +  "\" directory...");
-
-          BufferedReader reader = null;
-          BufferedWriter writer = null;
-
-          try {
-            reader = new BufferedReader(new FileReader(fromFile));
-            writer = new BufferedWriter(new FileWriter(toFile));
-
-            boolean done = false;
-
-            while(!done) {
-              String line = reader.readLine();
-
-              if(line == null) {
-                done = true;
-              }
-              else {
-                writer.write(StringUtil.substituteProperties(line, "@", "@"));
-
-                if(isUnixFile) {
-                  writer.write("\n");
-                }
-                else {
-                  writer.newLine();
-                }
-              }
-            }
-          }
-          finally {
-            if(reader != null) {
-              reader.close();
-            }
-
-            if(writer != null) {
-              writer.close();
-            }
-          }
-        }
-      }
+      copyConfigFiles("src/main/config");
     }
     catch (Exception e) {
       throw new LauncherException(e);
     }
 
     System.out.println("JLaunchPad is installed.");
+  }
+
+  private void install(String name) throws Exception {
+    System.out.println("Installing \"" + name + "\" project...");
+
+    installer.install("projects/" + name, false);
+  }
+
+  private void copyConfigFiles(String dir) throws IOException {
+    File[] files = new File(dir).listFiles();
+
+    String launcherHome = System.getProperty("launcher.home");
+    File launcherHomeFile = new File(launcherHome);
+
+    if (!launcherHomeFile.exists()) {
+      launcherHomeFile.mkdirs();
+    }
+
+    for (File fromFile : files) {
+      if (fromFile.exists() && !fromFile.isHidden() && !fromFile.isDirectory()) {
+        File toFile = new File(launcherHomeFile.toString(), fromFile.getName());
+
+        if(!fromFile.getName().equals("proxies-segment.xml")) {
+          System.out.println("Copying \"" + fromFile.getName() + "\" file to \"" + toFile.getParent() + "\" directory...");
+
+          copy(fromFile, toFile);
+        }
+      }
+    }
+  }
+
+  private void copy(File fromFile, File toFile) throws IOException {
+    String extension = FileUtil.getExtension(fromFile);
+
+    boolean isUnixFile = (extension != null && extension.equals("sh"));
+
+    BufferedReader reader = null;
+    BufferedWriter writer = null;
+
+    try {
+      reader = new BufferedReader(new FileReader(fromFile));
+      writer = new BufferedWriter(new FileWriter(toFile));
+
+      boolean done = false;
+
+      while (!done) {
+        String line = reader.readLine();
+
+        if (line == null) {
+          done = true;
+        } else {
+          writer.write(StringUtil.substituteProperties(line, "@", "@"));
+
+          if (isUnixFile) {
+            writer.write("\n");
+          } else {
+            writer.newLine();
+          }
+        }
+      }
+    }
+    finally {
+      if (reader != null) {
+        reader.close();
+      }
+
+      if (writer != null) {
+        writer.close();
+      }
+    }
   }
 
   /**
