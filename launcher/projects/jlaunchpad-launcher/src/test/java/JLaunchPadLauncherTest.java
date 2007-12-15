00317a551af090b8a44cddf2317491f20bacbeeb
shvets <shvets@b101193a-aa37-0410-b78f-865dd12e72d7>
2007-12-15 15:30:12 +0000

    git-svn-id: svn://svn.code.sf.net/p/jlaunchpad/code/trunk@85 b101193a-aa37-0410-b78f-865dd12e72d7

---
+++ launcher/projects/jlaunchpad-launcher/src/test/java/JLaunchPadLauncherTest.java
@@ -0,0 +1,111 @@
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+import org.codehaus.classworlds.ClassWorld;
+import org.sf.jlaunchpad.DepsLauncher;
+import org.sf.jlaunchpad.JLaunchPadLauncher;
+import org.sf.jlaunchpad.core.LauncherCommandLineParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JLaunchPadLauncherTest extends TestCase {
+
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    System.setProperty("repository.home", "c:/maven-repository");
+    System.setProperty("launcher.home", "c:/launcher");
+  }
+
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  public void testCreateJLaunchPad() {
+   // String repositoryHome = System.getProperty("repository.home");
+
+    String[] args = new String[]{};
+
+    ClassWorld classWorld = new ClassWorld();
+
+    LauncherCommandLineParser parser = new LauncherCommandLineParser();
+
+    try {
+      DepsLauncher launcher = new DepsLauncher(parser, args, classWorld);
+
+      launcher.configure(Thread.currentThread().getContextClassLoader());
+
+      // 1. download dependency
+      launcher.resolveDependencies("bsh", "bsh", "2.0b5");
+      // 2. have it on CLASSPATH
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+
+  public void testCreateJLaunchPadLauncher() {
+    String[] args = new String[]{};
+
+    try {
+      ClassWorld classWorld = new ClassWorld();
+
+      // LauncherCommandLineParser parser = new LauncherCommandLineParser();
+
+/*      List<String> depsFileNames = new ArrayList<String>();
+
+      depsFileNames.add("C:/maven-repository/org/sf/scriptlandia/beanshell-starter/1.0.0/beanshell-starter-1.0.0.pom ");
+      parser.getCommandLine().put("main.class.name", "bsh.Interpreter");
+      parser.getCommandLine().put("deps.file.name", depsFileNames);
+*/
+
+      LauncherCommandLineParser parser = new LauncherCommandLineParser();
+
+      JLaunchPadLauncher launcher = new JLaunchPadLauncher(parser, args, classWorld);
+
+      launcher.configure(Thread.currentThread().getContextClassLoader());
+
+      launcher.launch();
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+
+  public void testInstallProject() {
+ //   String repositoryHome = System.getProperty("repository.home");
+
+    String[] args = new String[]{
+        "-basedir", "projects/jlaunchpad-launcher/src/test/resources/test-data1",
+        "-build.required", "false"
+    };
+
+    List<String> depsFileNames = new ArrayList<String>();
+
+    depsFileNames.add( "projects/jlaunchpad-launcher/pom.xml ");
+
+    LauncherCommandLineParser parser = new LauncherCommandLineParser();
+    parser.getCommandLine().put("main.class.name", "org.sf.pomreader.ProjectInstaller");
+    parser.getCommandLine().put("deps.file.name", depsFileNames);
+
+    try {
+      ClassWorld classWorld = new ClassWorld();
+
+      JLaunchPadLauncher launcher = new JLaunchPadLauncher(parser, args, classWorld);
+
+      launcher.configure(Thread.currentThread().getContextClassLoader());
+
+      launcher.launch();
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+
+  public static void main(String[] args) {
+    TestSuite suite = new TestSuite();
+
+    suite.addTestSuite(JLaunchPadLauncherTest.class);
+
+    TestRunner.run(suite);
+  }
+
+}
